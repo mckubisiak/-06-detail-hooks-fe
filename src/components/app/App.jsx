9c1c2ac2106252cbb1475d95ee88fbb2200370d8
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-16 08:58:53 -0700

    created charater component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import RickAndMortyApi from '../../containers/RickAndMortyApi';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <RickAndMortyApi />;
 }
---
+++ src/containers/RickAndMortyApi.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const RickAndMortyApi = () => {
+  return <>Howdy </>;
+};
+
+export default RickAndMortyApi;
